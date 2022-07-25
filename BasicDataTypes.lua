bc705a2aba7f8d4ab874648b142b18313c4bcc68
RyanHeDev <86432912+RyanHeDev@users.noreply.github.com>
2022-07-25 07:09:54 -0700

    Create BasicDataTypes.lua

---
+++ BasicDataTypes.lua
@@ -0,0 +1,24 @@
+Data types
+Basic Lua types	
+nil · boolean · number · string · function · userdata · table
+Roblox types	
+Colors	
+BrickColor · Color3 · ColorSequence · ColorSequenceKeypoint
+Sequences	
+NumberSequence · NumberSequenceKeypoint
+Events	
+RBXScriptSignal · RBXScriptConnection
+Vectors	
+Vector2 · Vector3 · Vector2int16 · Vector3int16
+Classes	
+Instance · Other classes
+Chat-related types	
+ChatService · ChatBar · ChatChannel · ChatChannelUI · ChatChannelsBar · ChatMessage · ChatMessageLogDisplay · ChatSpeaker · ChatWindow
+Internal types	
+BinaryString · ProtectedString · QDir · QFont · Tuple · Variant
+Other types	
+Axes · CFrame/CoordinateFrame · Content · DockWidgetPluginGuiInfo · Enum · Faces · NumberRange · PathWaypoint · PhysicalProperties · Random · Ray · RbxLibrary · Rect · Region3 · Region3int16 · StamperController · UDim · UDim2 · TweenInfo
+C++ types	
+bool · double · float · int · int64 · void
+  
+  check them all in: https://roblox.fandom.com/wiki/Data_type 
